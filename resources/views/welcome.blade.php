c0479b9e2cb8bd6ef0acf278c858f6d93b2c8c93
Stefan Zweifel <hello@stefanzweifel.io>
2021-09-10 10:30:05 +0200

    Final Touches

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel App Key Generator</title>
+    <title>Laravel Encryption Key Generator</title>
 
     <script src="https://unpkg.com/tailwindcss-jit-cdn"></script>
 </head>
